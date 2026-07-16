b99c1be59e937ef853245ca9c597db00002549b5
Ali Hegazy <alihegazy19@outlook.com>
2026-07-16 15:21:57 +0300

    Add authentication layer on index page

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
 Route::get('/register', [RegisterController::class,'index'])->middleware('guest');
-Route::get('/login', [SessionController::class,'index'])->middleware('guest');
+Route::get('/login', [SessionController::class,'index'])->name('login')->middleware('guest');
