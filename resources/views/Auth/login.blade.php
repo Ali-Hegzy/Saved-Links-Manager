5bda2e5df207526c9384e0eb52f6911efab2cacf
Ali Hegazy <alihegazy19@outlook.com>
2026-07-16 15:14:37 +0300

    Add login page

---
+++ resources/views/Auth/login.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-form action='/login'>
+        <x-form.header title="Login" description="Glad to see you manage again."/>
+
+        <x-form.field name='email' type='email'/>
+
+        <x-form.field name='Password' type='password'/>
+
+        <button class="p-2 bg-primary text-text-main rounded-2xl hover:bg-secondary transition hover:text-muted w-fit m-auto cursor-pointer">Login</button>
+    </x-form>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::get('/register', [RegisterController::class,'index'])->middleware('guest');
+Route::get('/login', [SessionController::class,'index'])->middleware('guest');
