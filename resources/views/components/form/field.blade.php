70d6fa1b1f1c3a148f5a83aaef00b4be02aaa314
Ali Hegazy <alihegazy19@outlook.com>
2026-07-17 17:40:36 +0300

    Add ability to view old data of the request

--- resources/views/components/form/field.blade.php
+++ resources/views/components/form/field.blade.php
@@ -10,6 +10,7 @@
         id='{{ $name }}'
         name='{{ $name }}'
         class='outline-none p-1 pl-2 bg-primary-dimmed rounded-xl hover:bg-primary focus:bg-primary transition hover:text-text-main focus:text-text-main text-muted'
+        value="{{ old($name) }}"
         >
 
     @error($name)
