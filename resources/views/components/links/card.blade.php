14ba3b3f2b3d428a9bb33564bf7d064bd7029274
Ali Hegazy <alihegazy19@outlook.com>
2026-08-22 20:38:27 +0300

    Add the card component and the ability to see all saved links

---
+++ resources/views/components/links/card.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'title',
+    'description',
+    'site',
+    'status',
+    'link',
+])
+<div class="card bg-bg-card py-2 px-3 rounded-2xl text-text-main border-primary-dimmed border-2">
+    <h2 class="text-2xl">{{ $title }}</h2>
+    <p class="line-clamp-1">{{ $description }}</p>
+    <p>Site : {{ $site }}</p>
+    <div class="flex justify-between">
+        <p>Watched : {{ $status ? 'Yes' : 'No' }}</p>
+        <a href="{{ $link }}" target="_blank" class="underline text-red-500"> Watch</a>
+    </div>
+</div>
