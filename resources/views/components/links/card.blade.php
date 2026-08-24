b837f387fce9a3f322cb499782b70efcc249c7ed
Ali Hegazy <alihegazy19@outlook.com>
2026-08-24 04:31:46 +0300

    Make card as a seperate ui component

--- resources/views/components/links/card.blade.php
+++ resources/views/components/links/card.blade.php
@@ -5,12 +5,17 @@
     'status',
     'url',
 ])
-<div class="card bg-bg-card py-2 px-3 rounded-2xl text-text-main border-primary-dimmed border-2">
+
+<x-ui.card>
     <h2 class="text-2xl">{{ $title }}</h2>
     <p class="line-clamp-1">{{ $description }}</p>
     <p>Site : {{ $site }}</p>
     <div class="flex justify-between">
         <p>Watched : {{ $status ? 'Yes' : 'No' }}</p>
-        <a href="{{ $url }}" target="_blank" class="underline text-red-500">Watch</a>
+        <div class="links">
+            <a href="link/{{ $id }}" class="underline text-amber-500">View Link</a>
+            <a href="{{ $url }}" target="_blank" class="underline text-red-500">Watch</a>
+        </div>
     </div>
-</div>
+</x-ui.card>
+
---
+++ resources/views/components/ui/card.blade.php
@@ -0,0 +1,4 @@
+<div
+{{ $attributes->merge(['class' => 'card bg-bg-card py-2 px-3 rounded-2xl text-text-main border-primary-dimmed border-2']) }} >
+    {{ $slot }}
+</div>
