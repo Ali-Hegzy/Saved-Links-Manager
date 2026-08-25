e48a0decf1c86a8f0bdc4ba0cb97711eb3a31c11
Ali Hegazy <alihegazy19@outlook.com>
2026-08-26 02:36:03 +0300

    Enhance the link card view code

--- resources/views/components/links/card.blade.php
+++ resources/views/components/links/card.blade.php
@@ -1,20 +1,15 @@
 @props([
-    'title',
-    'description',
-    'site',
-    'status',
-    'url',
-    'id',
+    'link'
 ])
 <x-ui.card>
-    <h2 class="text-2xl">{{ $title }}</h2>
-    <p class="line-clamp-1">{{ $description }}</p>
-    <p>Site : {{ $site }}</p>
+    <h2 class="text-2xl">{{ $link->title }}</h2>
+    <p class="line-clamp-1">{{ $link->description }}</p>
+    <p>Site : {{ $link->site }}</p>
     <div class="flex justify-between">
-        <p>Watched : {{ $status ? 'Yes' : 'No' }}</p>
+        <p>Watched : {{ $link->status ? 'Yes' : 'No' }}</p>
         <div class="links">
-            <a href="/link/{{ $id }}" class="underline text-amber-500">View Link</a>
-            <a href="{{ $url }}" target="_blank" class="underline text-red-500">Watch</a>
+            <a href="/link/{{ $link->id }}" class="underline text-amber-500">View Link</a>
+            <a href="{{ $link->url }}" target="_blank" class="underline text-red-500">Watch</a>
         </div>
     </div>
 </x-ui.card>
