66f8743f28f9b845dbf71a6483353e12c0aa68e6
Ali Hegazy <alihegazy19@outlook.com>
2026-09-03 08:25:10 +0300

    Add Vertical Kebab Menu to the card

--- resources/views/components/links/card.blade.php
+++ resources/views/components/links/card.blade.php
@@ -5,7 +5,17 @@
     <div class="upper flex flex-row justify-between">
         <h2 class="text-2xl line-clamp-1">{{ $link->title }}</h2>
         <div x-data="{ open: false }" class="relative flex">
-            <button @click="open = ! open" class="cursor-pointer">...</button>
+            <button @click="open = ! open" class="cursor-pointer">
+                {{-- This SVG is AI Generated --}}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
+                    <!-- Top Dot -->
+                    <circle cx="12" cy="5" r="2" />
+                    <!-- Center Dot -->
+                    <circle cx="12" cy="12" r="2" />
+                    <!-- Bottom Dot -->
+                    <circle cx="12" cy="19" r="2" />
+                </svg>
+            </button>
 
             <div x-cloak x-show="open" @click.outside="open = false">
                 <x-ui.card class="absolute right-0 top-8 bg-bg-main">
