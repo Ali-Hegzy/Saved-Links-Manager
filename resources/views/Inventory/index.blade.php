f646693ef1a441c37f12c4d7457053f98931b223
Ali Hegazy <alihegazy19@outlook.com>
2026-09-03 14:37:42 +0300

    Vertical Kebab Menu to a separate component

--- resources/views/Inventory/index.blade.php
+++ resources/views/Inventory/index.blade.php
@@ -6,44 +6,27 @@
                         <h2 class="text-3xl">
                             {{ $inventory->name }}
                         </h2>
-                        <div x-data="{ open: false }" class="relative flex">
-                            <button @click="open = ! open" class="cursor-pointer">
-                                {{-- This SVG is AI Generated --}}
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-                                    <!-- Top Dot -->
-                                    <circle cx="12" cy="5" r="2" />
-                                    <!-- Center Dot -->
-                                    <circle cx="12" cy="12" r="2" />
-                                    <!-- Bottom Dot -->
-                                    <circle cx="12" cy="19" r="2" />
-                                </svg>
-                            </button>
-
-                            <div x-cloak x-show="open" @click.outside="open = false">
-                                <x-ui.card class="absolute right-0 top-8 bg-bg-main">
-                                    <ul class="flex flex-col gap-2">
-                                        <li>
-                                            <a href="{{ route('inventories.show', $inventory) }}">
-                                                View
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ route('inventories.edit', $inventory) }}">
-                                                Edit
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <form action="{{ route('inventories.destroy', $inventory) }}" method="POST">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button class="cursor-pointer">DELETE</button>
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </x-ui.card>
-                            </div>
-
-                        </div>
+                        <x-ui.vKebabMenu>
+                            <ul class="flex flex-col gap-2">
+                                <li>
+                                    <a href="{{ route('inventories.show', $inventory) }}">
+                                        View
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('inventories.edit', $inventory) }}">
+                                        Edit
+                                    </a>
+                                </li>
+                                <li>
+                                    <form action="{{ route('inventories.destroy', $inventory) }}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button class="cursor-pointer">DELETE</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </x-ui.vKebabMenu>
                     </div>
                     <p>{{ $inventory->description }}</p>
                 </x-ui.card>
--- resources/views/components/links/card.blade.php
+++ resources/views/components/links/card.blade.php
@@ -4,34 +4,17 @@
 <x-ui.card>
     <div class="upper flex flex-row justify-between">
         <h2 class="text-2xl line-clamp-1">{{ $link->title }}</h2>
-        <div x-data="{ open: false }" class="relative flex">
-            <button @click="open = ! open" class="cursor-pointer">
-                {{-- This SVG is AI Generated --}}
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-                    <!-- Top Dot -->
-                    <circle cx="12" cy="5" r="2" />
-                    <!-- Center Dot -->
-                    <circle cx="12" cy="12" r="2" />
-                    <!-- Bottom Dot -->
-                    <circle cx="12" cy="19" r="2" />
-                </svg>
-            </button>
-
-            <div x-cloak x-show="open" @click.outside="open = false">
-                <x-ui.card class="absolute right-0 top-8 bg-bg-main">
-                    <ul class="flex flex-col gap-2">
-                        <li>
-                            <form action="/link/{{ $link->id }}/delete" method="POST">
-                                @method('DELETE')
-                                @csrf
-                                <button class="cursor-pointer">DELETE</button>
-                            </form>
-                        </li>
-                    </ul>
-                </x-ui.card>
-            </div>
-
-        </div>
+        <x-ui.vKebabMenu>
+            <ul class="flex flex-col gap-2">
+                <li>
+                    <form action="/link/{{ $link->id }}/delete" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button class="cursor-pointer">DELETE</button>
+                    </form>
+                </li>
+            </ul>
+        </x-ui.vKebabMenu>
     </div>
     <p class="line-clamp-1">{{ $link->description }}</p>
     <p>Site : {{ $link->site }}</p>
