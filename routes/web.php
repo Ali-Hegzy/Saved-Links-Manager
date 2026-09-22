34b211444b5b976dee0044ca525b7fed28079407
Ali Hegazy <alihegazy19@outlook.com>
2026-09-22 20:03:19 +0300

    Reorganize web.php

--- routes/web.php
+++ routes/web.php
@@ -13,39 +13,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', [RegisterController::class,'index'])->middleware('guest');
-Route::post('/register', [RegisterController::class,'create'])->middleware(['guest', 'throttle:login']);
+Route::middleware('guest')->group(function (){
+    Route::get('/register', [RegisterController::class,'index']);
+    Route::post('/register', [RegisterController::class,'create'])->middleware('throttle:login');
 
-Route::get('/login', [SessionController::class,'index'])->name('login')->middleware('guest');
-Route::post('/login', [SessionController::class,'create'])->middleware(['guest', 'throttle:login']);
-
-Route::delete('/logout', [SessionController::class,'destroy'])->middleware('auth');
+    Route::get('/login', [SessionController::class,'index'])->name('login');
+    Route::post('/login', [SessionController::class,'create'])->middleware('throttle:login');
+});
 
 Route::middleware('auth')->group(function (){
+    Route::delete('/logout', [SessionController::class,'destroy']);
+
+    Route::get('/profile',[UserController::class,'index']);
+
+    // Links Resource
     Route::put('links/{link}', [LinkController::class,'update'])
         ->middleware('can:update,link')
         ->name('links.update');
-
     Route::resource('links', LinkController::class)->except(['update']);
-});
 
-Route::get('/profile',[UserController::class,'index'])->middleware('auth');
-
-Route::middleware('auth')->group(function () {
+    // Sites Resource
     Route::put('sites/{site}', [SiteController::class,'update'])
         ->middleware('can:update,site')
         ->name('sites.update');
-
     Route::resource('sites', SiteController::class)->except(['create', 'show', 'update']);
-});
 
-Route::middleware('auth')->group(function (){
+    // Inventories Resource
     Route::put('inventories/{inventory}', [InventoryController::class,'update'])
         ->middleware('can:update,inventory')
         ->name('inventories.update');
-
     Route::resource('inventories',InventoryController::class)->except(['update']);
-});
 
-Route::post('/invItems', [inventoryItemController::class, 'store'])->middleware('auth')->name('inventoryItems.store');
-Route::delete('/invItems', [inventoryItemController::class, 'destroy'])->middleware('auth')->name('inventoryItems.destroy');
+    Route::post('/invItems', [inventoryItemController::class, 'store'])->name('inventoryItems.store');
+    Route::delete('/invItems', [inventoryItemController::class, 'destroy'])->name('inventoryItems.destroy');
+});
